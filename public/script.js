2c300e1e3754516eb27d2b76d58d03d2bd21a734
Ayush Jain <ayushshreeshreemal@gmail.com>
2022-05-06 14:42:58 -0700

    finished adding rooms

--- public/script.js
+++ public/script.js
@@ -1,27 +1,34 @@
 const socket = io('http://localhost:3000')
 const messageContainer = document.getElementById('message-container')
+const roomContainer = document.getElementById('room-container')
 const messageForm = document.getElementById('send-container')
 const messageInput = document.getElementById('message-input')
 
 if (messageForm != null) {
   const name = prompt('What is your name?')
   appendMessage('You joined')
-  socket.emit('new-user', name)
+  socket.emit('new-user', roomName, name)
 
   messageForm.addEventListener('submit', e => {
-    //Prevent Page Refresh so that we dont lose chat messages
-      e.preventDefault()
-      const message = messageInput.value
-      appendMessage(`You: ${message}`)
-    //   send information from client to the server
-      socket.emit('send-chat-message', message)
-    //   Empty the input field after sending message
-      messageInput.value = ''
-    })
-  }
+    e.preventDefault()
+    const message = messageInput.value
+    appendMessage(`You: ${message}`)
+    socket.emit('send-chat-message', roomName, message)
+    messageInput.value = ''
+  })
+}
+
+socket.on('room-created', room => {
+  const roomElement = document.createElement('div')
+  roomElement.innerText = room
+  const roomLink = document.createElement('a')
+  roomLink.href = `/${room}`
+  roomLink.innerText = 'join'
+  roomContainer.append(roomElement)
+  roomContainer.append(roomLink)
+})
 
 socket.on('chat-message', data => {
-    // Show message in the DOM
   appendMessage(`${data.name}: ${data.message}`)
 })
 
@@ -33,7 +40,6 @@ socket.on('user-disconnected', name => {
   appendMessage(`${name} disconnected`)
 })
 
-// Add message to the DOM
 function appendMessage(message) {
   const messageElement = document.createElement('div')
   messageElement.innerText = message
--- server.js
+++ server.js
@@ -8,43 +8,51 @@ app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 
-const rooms = {name: {}}
+const rooms = { }
 
 app.get('/', (req, res) => {
-  res.render('index', {rooms: rooms})
+  res.render('index', { rooms: rooms })
 })
 
-app.post('/room', (res, req) => {
+app.post('/room', (req, res) => {
   if (rooms[req.body.room] != null) {
     return res.redirect('/')
   }
   rooms[req.body.room] = { users: {} }
   res.redirect(req.body.room)
   // Send message that new room was created
+  io.emit('room-created', req.body.room)
 })
 
 app.get('/:room', (req, res) => {
-  res.render('room', {roomName: req.params.room})
+  if (rooms[req.params.room] == null) {
+    return res.redirect('/')
+  }
+  res.render('room', { roomName: req.params.room })
 })
 
 server.listen(3000)
 
-const users = {}
-
-// gives each user its own socket
 io.on('connection', socket => {
-  socket.on('new-user', name => {
-    users[socket.id] = name
-    // Every time someone connects, we are sending a message down to the client
-    socket.broadcast.emit('user-connected', name)
+  socket.on('new-user', (room, name) => {
+    socket.join(room)
+    rooms[room].users[socket.id] = name
+    socket.to(room).broadcast.emit('user-connected', name)
   })
-//   Handling event
-  socket.on('send-chat-message', message => {
-    //   send information to every single user connected to the server except the one who sent it
-    socket.broadcast.emit('chat-message', { message: message, name: users[socket.id] })
+  socket.on('send-chat-message', (room, message) => {
+    socket.to(room).broadcast.emit('chat-message', { message: message, name: rooms[room].users[socket.id] })
   })
   socket.on('disconnect', () => {
-    socket.broadcast.emit('user-disconnected', users[socket.id])
-    delete users[socket.id]
+    getUserRooms(socket).forEach(room => {
+      socket.to(room).broadcast.emit('user-disconnected', rooms[room].users[socket.id])
+      delete rooms[room].users[socket.id]
+    })
   })
 })
+
+function getUserRooms(socket) {
+  return Object.entries(rooms).reduce((names, [name, room]) => {
+    if (room.users[socket.id] != null) names.push(name)
+    return names
+  }, [])
+}
