79de9d17abf13c9276cf7afc0871dc223f59aaff
Ayush Jain <ayushshreeshreemal@gmail.com>
2023-06-06 13:07:35 -0700

    v

--- server.js
+++ server.js
@@ -62,4 +62,9 @@ function getUserRooms(socket) {
   }, [])
 }
 
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
 module.exports.handler = serverless(app);
